b2e77cc730c08477f3e11b63e2ff1a54cf6f5aaf
Ife-PopOs <sunmolaife@gmail.com>
2022-12-24 13:23:32 -0700

    Initial Commit 1 - For restarting and switching to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginFormComponent } from './landing-page/login-form/login-form.component';
+import { RegisterFormComponent } from './landing-page/register-form/register-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
