fadfa394cc794a7e49b194309168cfe943f582f5
Ife-PopOs <sunmolaife@gmail.com>
2022-12-26 15:14:20 -0700

    Finished styling add new deadline

--- src/app/user-landing-page/user-landing-page.component.ts
+++ src/app/user-landing-page/user-landing-page.component.ts
@@ -1,29 +1,44 @@
-import {Component, TemplateRef} from '@angular/core';
-import {NgbDateStruct, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {Option} from "./Option";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
-  selector: 'app-user-landing-page',
-  templateUrl: './user-landing-page.component.html',
-  styleUrls: ['./user-landing-page.component.scss']
+	selector: 'app-user-landing-page',
+	templateUrl: './user-landing-page.component.html',
+	styleUrls: ['./user-landing-page.component.scss']
 })
-export class UserLandingPageComponent {
-  minDate: any = {year: 0, month: 0, day: 0};
-  minTime: any = {hour: 0, minute: 0};
-  model: NgbDateStruct | undefined;
+export class UserLandingPageComponent implements OnInit {
+	minDate: any = {year: 0, month: 0, day: 0};
 
-  constructor(private modalService: NgbModal) {
-  }
+	options: Option[] = []
+	title: string = "";
+	dueDate: string = "";
+	dueTime: string = "";
 
-  openAddDeadlineModal(content: TemplateRef<NgbDateStruct>) {
-    this.minTime = {
-      hour: new Date().getHours(),
-      minute: new Date().getMinutes()
-    }
-    this.minDate = {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth() + 1,
-      day: new Date().getDate()
-    }
-    this.modalService.open(content, {centered: true});
-  }
+
+	constructor(private modalService: NgbModal) {
+	}
+
+	ngOnInit(): void {
+		this.options = [
+			new Option("30 Minutes", true),
+			new Option("1 Hour", false),
+			new Option("2 Hours", false),
+			new Option("3 Hours", false),
+			new Option("4 Hours", false),
+		]
+	}
+
+	openAddDeadlineModal(content: TemplateRef<any>) {
+		const currentDate = new Date();
+		this.minDate = {
+			year: currentDate.getFullYear(),
+			month: currentDate.getMonth() + 1,
+			day: currentDate.getDate()
+		}
+		this.modalService.open(content, {centered: true});
+	}
+
 }
+
