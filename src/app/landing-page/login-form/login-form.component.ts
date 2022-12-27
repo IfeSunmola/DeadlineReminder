0ab713e0e8012bfabe934660b5a329a0eb17e817
Ife-PopOs <sunmolaife@gmail.com>
2022-12-26 20:40:09 -0700

    Added 2 way bindings

--- src/app/landing-page/login-form/login-form.component.ts
+++ src/app/landing-page/login-form/login-form.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoginData} from "./login-data";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+	selector: 'app-login-form',
+	templateUrl: './login-form.component.html',
+	styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
-
+	loginData: LoginData = new LoginData();
 }
