be62020e6c2d888585b5600c09bf0479b1fcc217
Ife-PopOs <sunmolaife@gmail.com>
2022-12-26 20:31:25 -0700

    Deleted files

--- src/app/landing-page/register-form/register-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RegisterData } from './register-data';
-
-describe('RegisterData', () => {
-  it('should create an instance', () => {
-    expect(new RegisterData()).toBeTruthy();
-  });
-});
