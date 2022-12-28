ac10bc7d66424045b3945d7f18e860b21ec04c56
Ife-PopOs <sunmolaife@gmail.com>
2022-12-28 04:25:42 -0700

    Added AuthAccount and AuthAccountService

---
+++ src/main/java/com/example/backend/security/AuthAccountService.java
@@ -0,0 +1,37 @@
+package com.example.backend.security;
+
+import com.example.backend.exceptions.EmailNotFoundException;
+import com.example.backend.repos.AccountRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class for the AuthAccount class.
+ * <p>
+ * This class is mainly used by spring security to get the currently authenticated account
+ *
+ * @author Ife Sunmola
+ */
+@AllArgsConstructor
+@Service
+public class AuthAccountService implements UserDetailsService {
+	private final AccountRepo accountRepo;
+
+	/**
+	 * Method to find a user by their username. In our case, by their email
+	 *
+	 * @param email the email to load the user by
+	 * @return the AuthAccount if the email associated to it was found
+	 * @throws EmailNotFoundException if the email was not found in the database
+	 * @author Ife Sunmola
+	 */
+	@Override
+	public AuthAccount loadUserByUsername(String email) throws EmailNotFoundException {
+		return new AuthAccount(accountRepo.findByEmail(email)
+				.orElseThrow(
+						() -> new EmailNotFoundException("Email '" + email + "' was not found")
+				)
+		);
+	}
+}
