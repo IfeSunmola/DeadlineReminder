5d09c250f816b20137b2bb6bb8e0bbc2f2e76d75
Ife-PopOs <sunmolaife@gmail.com>
2022-12-28 04:11:16 -0700

    Added file

---
+++ src/main/java/com/example/backend/repos/AccountRepo.java
@@ -0,0 +1,19 @@
+package com.example.backend.repos;
+
+import com.example.backend.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Data access layer. This interface provides the methods we use to get information from our database
+ *
+ * @author Ife Sunmola
+ */
+@Repository
+public interface AccountRepo extends JpaRepository<Account, Long> {
+	Optional<Account> findByEmail(String email);
+
+	boolean existsByEmail(String email);
+}
