fbf2f2e36f834fe9787a1fc2a083711b83dedc17
Ife-PopOs <sunmolaife@gmail.com>
2022-12-28 17:41:02 -0700

    backend - Removed password validation

--- backend/src/main/java/com/example/backend/transfer_objects/LoginData.java
+++ backend/src/main/java/com/example/backend/transfer_objects/LoginData.java
@@ -1,6 +1,5 @@
 package com.example.backend.transfer_objects;
 
-import com.example.backend.transfer_objects.validations.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,7 +19,6 @@ public class LoginData {
 	@Email(message = "A valid email is required")
 	private String email;
 
-	@ValidPassword
 	private String password;
 
 	@NotNull(message = "True or False is required")
