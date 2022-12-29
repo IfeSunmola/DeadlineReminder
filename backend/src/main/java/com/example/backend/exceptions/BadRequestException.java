80a3202b41de855fa6351172811046c29c4578ce
Ife-PopOs <sunmolaife@gmail.com>
2022-12-28 17:51:50 -0700

    backend - Added logger

--- backend/src/main/java/com/example/backend/exceptions/BadRequestException.java
+++ backend/src/main/java/com/example/backend/exceptions/BadRequestException.java
@@ -1,12 +1,16 @@
 package com.example.backend.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * This exception will be thrown when a request from the client is invalid
  *
  * @author Ife Sunmola
  */
+@Slf4j
 public class BadRequestException extends RuntimeException {
 	public BadRequestException(String message) {
 		super(message);
+		log.error(message);
 	}
 }
