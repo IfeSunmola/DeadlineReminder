03636bb6b5404dc47ce34b41409a063272746d0f
Ife-PopOs <sunmolaife@gmail.com>
2022-12-29 00:18:39 -0700

    backend - Added endpoints for creating new user and generating jwts

--- backend/src/main/java/com/example/backend/controllers/AuthController.java
+++ backend/src/main/java/com/example/backend/controllers/AuthController.java
@@ -1,33 +1,29 @@
 package com.example.backend.controllers;
 
 import com.example.backend.models.Account;
-import com.example.backend.security.AuthAccountService;
 import com.example.backend.services.AccountService;
-import com.example.backend.transfer_objects.LoginData;
 import com.example.backend.transfer_objects.RegisterData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Handles requests to /accounts/auth
+ * Handles requests to /auth
  *
  * @author Ife Sunmola
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/accounts/auth")
+@RequestMapping("/auth")
 @Slf4j
 public class AuthController {
-
-	private final AuthAccountService authAccountService;
-
 	private final AccountService accountService;
 
 	@PostMapping("/register")
@@ -36,16 +32,11 @@ public class AuthController {
 		return new ResponseEntity<>(accountService.createAccount(registerData), HttpStatus.CREATED);
 	}
 
-	@PostMapping("/login")
-	public String doLogin(@RequestBody @Valid LoginData loginData) {
-		return accountService.generateToken(loginData);
-	}
-
-	@PostMapping("/token")
+	@PostMapping("/generate-token")
 	public String token(Authentication auth) {
-		log.debug("Token requested for user: {}", auth.getName());
+		log.info("Token requested for user: {}", auth.getName());
 		String token = accountService.generateToken(auth);
-		log.debug("Token granted: {}", token);
+		log.info("Token granted: {}\n", token);
 		return token;
 	}
 
@@ -54,7 +45,9 @@ public class AuthController {
 		return "<h1>NO AUTHENTICATION NEEDED</h1>";
 	}
 
+
 	@GetMapping("/logged-in")
+	@PreAuthorize("isAuthenticated()")
 	public String page2() {
 		return "<h1>YOU ARE LOGGED IN</h1>";
 	}
