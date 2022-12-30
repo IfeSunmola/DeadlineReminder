1882d799de7b1e37d57fc30aceace8636b861873
Ife-PopOs <sunmolaife@gmail.com>
2022-12-29 22:19:45 -0700

    changed the app title come from app-root

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+	selector: 'app-landing-page',
+	templateUrl: './landing-page.component.html',
+	styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
 
+	@Input() title!: string;
 }
