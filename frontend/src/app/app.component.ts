a72096c291fa7d18e3497fb6b77de9dfe690526e
Ife-PopOs <sunmolaife@gmail.com>
2023-01-01 20:31:27 -0700

    Added functionality to show error messages on all pages if the screen size is > x (width)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+	title = 'frontend';
+
+	screenWidth: number | undefined;
+	hasValidScreen: boolean = false;
+
+	ngOnInit(): void {
+		this.checkScreen();
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onWindowResize() {
+		this.checkScreen();
+	}
+
+	private checkScreen(): void {
+		const MAX_WIDTH = 768;
+		this.screenWidth = window.innerWidth;
+		this.hasValidScreen = this.screenWidth < MAX_WIDTH;
+	}
 }
