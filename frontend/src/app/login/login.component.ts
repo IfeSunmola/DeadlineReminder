fcb6c3637ae80aa3283926282cb5b84e6573bfc4
Ife-PopOs <sunmolaife@gmail.com>
2023-02-12 01:04:56 -0700

    Added login form validation + logic to send login data to the backend and receive jwt back

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,73 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {LoginData} from "../models/login-data";
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+	loginForm!: FormGroup
 
+	constructor(private authService: AuthService, private router: Router) {
+	}
+
+	formSubmitted() {
+		const loginData: LoginData = {email: this.email?.value, password: this.password?.value, stayLoggedIn: this.stayLoggedIn?.value}
+
+		this.authService.login(loginData).subscribe(
+			{
+				next: (response) => {
+					console.log("Response: " + response)
+					this.router.navigate(['/me']).then()
+				},
+				// handle unauthorized
+				error: (error) => {
+					console.log("Error: " + error.error)
+				}
+			}
+		)
+	}
+
+	ngOnInit(): void {
+		this.loginForm = new FormGroup(
+			{
+				email: new FormControl(
+					"sunmolaife@gmail.com",
+					{
+						validators: [
+							Validators.required,
+							Validators.email
+						]
+					}
+				),
+				password: new FormControl(
+					"password",
+					{
+						validators: [
+							Validators.required
+						]
+					}
+				),
+				stayLoggedIn: new FormControl(
+					false
+				)
+			}
+		)
+	}
+
+	get email() {
+		return this.loginForm.get('email');
+	}
+
+	get password() {
+		return this.loginForm.get('password')
+	}
+
+	get stayLoggedIn() {
+		return this.loginForm.get('stayLoggedIn')
+	}
 }
