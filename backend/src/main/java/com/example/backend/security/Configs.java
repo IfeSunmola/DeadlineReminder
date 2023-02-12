6bf50e8fe93be0d312ba6bf81911c72559a6c8da
Ife-PopOs <sunmolaife@gmail.com>
2023-02-11 23:42:40 -0700

    Disabled csrf and cors Added DaoAuthProvider

--- backend/src/main/java/com/example/backend/security/Configs.java
+++ backend/src/main/java/com/example/backend/security/Configs.java
@@ -3,6 +3,7 @@ package com.example.backend.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -13,8 +14,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class Configs {
+	private final AuthAccountService authAccountService;
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(authAccountService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
 	}
 }
--- backend/src/main/java/com/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,18 +15,20 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * @author Ife Sunmola
  */
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity()
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 	private static final String[] WHITELISTED_URLS = {
-			"/",
-			"/no-auth",
+			"/auth/**",
 	};
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+				// TODO: ENABLE CSRF
+				.cors(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers(new AntPathRequestMatcher("/h2/**")).permitAll()
 						.requestMatchers(WHITELISTED_URLS).permitAll()
