03131b485e8280505215e78002d3c964a01edde0
Ife-PopOs <sunmolaife@gmail.com>
2023-02-11 23:34:29 -0700

    Initial Commit

---
+++ frontend/src/app/AppConstants.ts
@@ -0,0 +1,10 @@
+export const API_URL = "http://localhost:8080";
+export const VERIFY_CODE_LENGTH = 5
+export const SUCCESS = "Success";
+export const INCORRECT = "Incorrect";
+export const EXPIRED = "Expired";
+export const INVALID_REQUEST = "InvalidRequest";
+export const ERROR = "Error";
+// TODO: Check if db can handle encrypted password of this long
+export const MIN_PASSWORD_LENGTH = 8
+export const MAX_PASSWORD_LENGTH = 250
