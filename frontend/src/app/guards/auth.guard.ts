d72b6b64815418db92997541b95ea468b2c25e9e
Ife-PopOs <sunmolaife@gmail.com>
2023-02-12 22:00:58 -0700

    changed to be inside guards directory

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
-import {AuthService} from "./services/auth.service";
+import {AuthService} from "../services/auth.service";
+
 
 @Injectable({
 	providedIn: 'root'
