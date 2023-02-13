a7613c27d29a88071a0a5bce2f35942d8d0b335c
Ife-PopOs <sunmolaife@gmail.com>
2023-02-12 20:55:15 -0700

    updated

--- frontend/src/app/AppConstants.ts
+++ frontend/src/app/AppConstants.ts
@@ -4,7 +4,7 @@ export const SUCCESS = "Success";
 export const INCORRECT = "Incorrect";
 export const EXPIRED = "Expired";
 export const INVALID_REQUEST = "InvalidRequest";
-export const ERROR = "Error";
 // TODO: Check if db can handle encrypted password of this long
 export const MIN_PASSWORD_LENGTH = 8
 export const MAX_PASSWORD_LENGTH = 250
+export const AUTH_TOKEN = "AuthToken"
