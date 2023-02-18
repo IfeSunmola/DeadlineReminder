72f1330041ad223d236cd31c8c21d0db9a68bad1
Ife-PopOs <sunmolaife@gmail.com>
2023-02-18 04:49:32 -0700

    Added invalid request message

--- frontend/src/app/AppConstants.ts
+++ frontend/src/app/AppConstants.ts
@@ -4,6 +4,7 @@ export const SUCCESS = "Success";
 export const INCORRECT = "Incorrect";
 export const EXPIRED = "Expired";
 export const INVALID_REQUEST = "InvalidRequest";
+export const INVALID_REQUEST_MESSAGE = "Invalid request";
 // TODO: Check if db can handle encrypted password of this long
 export const MIN_PASSWORD_LENGTH = 8
 export const MAX_PASSWORD_LENGTH = 250
--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {INVALID_REQUEST, INVALID_REQUEST_MESSAGE} from "../AppConstants";
 
 @Component({
 	selector: 'app-homepage',
 	templateUrl: './homepage.component.html',
 	styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+	invalidRequest = false;
+	readonly INVALID_REQUEST_MESSAGE = INVALID_REQUEST_MESSAGE
+
+	ngOnInit(): void {
+		this.invalidRequest = localStorage.getItem(INVALID_REQUEST) === "true"
+		localStorage.removeItem(INVALID_REQUEST)
+	}
 
 }
