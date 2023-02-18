dbd01a5d8687af596fdbe4b9ecf96d93af758507
Ife-PopOs <sunmolaife@gmail.com>
2023-02-18 14:08:05 -0700

    Added invalid passwword reset link message

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {INVALID_REQUEST, INVALID_REQUEST_MESSAGE} from "../AppConstants";
+import {INVALID_REQUEST, INVALID_REQUEST_MESSAGE, INVALID_RESET_LINK, INVALID_RESET_LINK_MESSAGE} from "../AppConstants";
 
 @Component({
 	selector: 'app-homepage',
@@ -9,10 +9,17 @@ import {INVALID_REQUEST, INVALID_REQUEST_MESSAGE} from "../AppConstants";
 export class HomepageComponent implements OnInit{
 	invalidRequest = false;
 	readonly INVALID_REQUEST_MESSAGE = INVALID_REQUEST_MESSAGE
+	// password reset link has expired or is not valid
+	invalidResetLink = false
+	readonly INVALID_RESET_LINK_MESSAGE = INVALID_RESET_LINK_MESSAGE
 
 	ngOnInit(): void {
 		this.invalidRequest = localStorage.getItem(INVALID_REQUEST) === "true"
 		localStorage.removeItem(INVALID_REQUEST)
+
+		this.invalidResetLink = localStorage.getItem(INVALID_RESET_LINK) === "true"
+		localStorage.removeItem(INVALID_RESET_LINK)
+
 	}
 
 }
