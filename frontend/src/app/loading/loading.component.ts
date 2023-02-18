cd10338d22e8f9b9f6b1f5673b862713f14ef15a
Ife-PopOs <sunmolaife@gmail.com>
2023-02-17 21:25:48 -0700

    Initial Commit

---
+++ frontend/src/app/loading/loading.component.ts
@@ -0,0 +1,25 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {LoadingService} from "../services/loading.service";
+
+@Component({
+	selector: 'app-loading',
+	templateUrl: './loading.component.html',
+	styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent implements OnInit {
+	showLoader = false;
+
+	constructor(private loadingService: LoadingService, private cdRef: ChangeDetectorRef) {
+	}
+
+	ngOnInit(): void {
+		this.init()
+	}
+
+	private init() {
+		this.loadingService.loadingObserver.subscribe((status) => {
+			this.showLoader = status === 'start'
+			this.cdRef.detectChanges()
+		})
+	}
+}
