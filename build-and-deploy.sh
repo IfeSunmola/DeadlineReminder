6c01c66db4d41b83f78cabb44319d72697d4b223
Ife-PopOs <sunmolaife@gmail.com>
2023-02-21 21:02:34 -0700

    updated to use cmd line args

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,17 +1,38 @@
+#!/bin/bash
 # Script to automate build + push
 az acr login --name ifesunmola
 
-# Starting with backend
-echo "Building backend img..."
-(cd backend || exit; docker build --no-cache  -t ifesunmola.azurecr.io/backend .)
-echo "Backend img built"
-echo "Pushing backend img..."
-docker push ifesunmola.azurecr.io/backend
+arg=$1
 
-# Next frontend
-read -rp "Did you change the api link in AppConstants.ts?"
-echo "Building frontend img..."
-(cd frontend || exit; docker build --no-cache -t ifesunmola.azurecr.io/frontend .)
-echo "Frontend img built"
-echo "Pushing frontend img..."
-docker push ifesunmola.azurecr.io/frontend
+run_backend(){
+      echo "Building backend img..."
+      (cd backend || exit; docker build --no-cache  -t ifesunmola.azurecr.io/backend .)
+      echo "Backend img built"
+      echo "Pushing backend img..."
+      docker push ifesunmola.azurecr.io/backend
+}
+
+run_frontend(){
+      read -rp "Did you change the api link in AppConstants.ts?"
+      echo "Building frontend img..."
+      (cd frontend || exit; docker build --no-cache -t ifesunmola.azurecr.io/frontend .)
+      echo "Frontend img built"
+      echo "Pushing frontend img..."
+      docker push ifesunmola.azurecr.io/frontend
+}
+
+if [ "$arg" = "frontend" ]; then
+      run_frontend
+      exit 0;
+fi
+
+if [ "$arg" = "backend" ]; then
+      run_backend
+      exit 0;
+fi
+
+if [ "$arg" = "" ]; then
+      run_backend
+      run_frontend
+      exit 0;
+fi
