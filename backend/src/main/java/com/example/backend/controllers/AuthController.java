fa32ca434857fc0285b1c907f1de4cb421696e7c
Ife-PopOs <sunmolaife@gmail.com>
2023-02-22 15:18:32 -0700

    Split AccountService into AuthService + AccountService

--- backend/src/main/java/com/example/backend/controllers/AuthController.java
+++ backend/src/main/java/com/example/backend/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.backend.controllers;
 
 import com.example.backend.services.AccountService;
+import com.example.backend.services.AuthService;
 import com.example.backend.transfer_objects.LoginData;
 import com.example.backend.transfer_objects.PasswordResetData;
 import com.example.backend.transfer_objects.RegisterData;
@@ -24,13 +25,14 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+	private final AuthService authService;
 	private final AccountService accountService;
 
 	@PostMapping(value = "/generate-token")
 	public ResponseEntity<Map<String, String>> generateToken(@RequestBody @Valid LoginData loginData) {
 		log.info("Token requested for user: {}", loginData.getEmail());
 		Map<String, String> result =
-				accountService.authenticateUser(loginData);
+				authService.authenticateUser(loginData);
 
 		if (result.containsKey("token")) {
 			log.info("Token granted: {}\n", result);
@@ -44,46 +46,46 @@ public class AuthController {
 	public ResponseEntity<Map<String, String>> register(@RequestBody @Valid RegisterData registerData, BindingResult result)
 			throws MethodArgumentNotValidException, NoSuchMethodException {
 
-		accountService.validateData(registerData, result); // throws exception if there are errors
-		Map<String, String> emailAndCodeId = accountService.createAccount(registerData);
+		authService.validateData(registerData, result); // throws exception if there are errors
+		Map<String, String> emailAndCodeId = authService.createAccount(registerData);
 		return new ResponseEntity<>(emailAndCodeId, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/register/verify")
 	public Map<String, String> confirmCode(@RequestBody VerifyCodeData data) {
-		return accountService.verifyAccount(data);
+		return authService.verifyAccount(data);
 	}
 
 	@PostMapping("/email-exists")
 	public boolean emailExists(@RequestBody String email) {
-		return accountService.emailExists(email);
+		return authService.emailExists(email);
 	}
 
 	@PostMapping("/send-verification-email")
 	public ResponseEntity<Map<String, String>> sendVerificationEmail(@RequestBody String email) {
 		log.info("Verification email requested for user: {}", email);
-		Map<String, String> emailAndCodeId = accountService.sendVerificationEmail(accountService.findByEmail(email));
+		Map<String, String> emailAndCodeId = authService.sendVerificationEmail(accountService.findByEmail(email));
 		return new ResponseEntity<>(emailAndCodeId, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/send-reset-password-email")
 	public HttpStatus sendResetPasswordEmail(@RequestBody String email) {
 		log.info("Reset password email requested for user: {}", email);
-		accountService.sendResetPasswordEmail(email);
+		authService.sendResetPasswordEmail(email);
 		return HttpStatus.CREATED;
 	}
 
 	@GetMapping("/reset-password/verify")
 	public String verifyPasswordResetCode(@RequestParam String token) {
 		log.info("Password reset code verification requested for code: {}", token);
-		return accountService.verifyPasswordResetToken(token); // returns the email associated with the code
+		return authService.verifyPasswordResetToken(token); // returns the email associated with the code
 	}
 
 	@PutMapping("/confirm-reset")
 	public Map<String, String> confirmPasswordReset(@RequestBody @Valid PasswordResetData passwordResetData, BindingResult result)
 			throws MethodArgumentNotValidException, NoSuchMethodException {
 		log.info("Reset password requested for user: {}", passwordResetData);
-		accountService.validatePasswordResetData(passwordResetData, result);  // throws exception if there are errors
-		return accountService.confirmPasswordReset(passwordResetData);
+		authService.validatePasswordResetData(passwordResetData, result);  // throws exception if there are errors
+		return authService.confirmPasswordReset(passwordResetData);
 	}
 }
