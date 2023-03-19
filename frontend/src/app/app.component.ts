4e2aeaa2e49a25eb345af79f99c83d28be2e850c
Ife-PopOs <sunmolaife@gmail.com>
2023-03-18 23:47:44 -0600

    renamed isValidScreen to small screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,21 +6,14 @@ import {Component, HostListener, OnInit} from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-	screenWidth: number | undefined;
-	hasValidScreen: boolean = false;
+	smallScreen = false;
 
 	ngOnInit(): void {
-		this.checkScreen();
+		this.smallScreen = window.innerWidth < 768;
 	}
 
 	@HostListener('window:resize', ['$event'])
 	onWindowResize() {
-		this.checkScreen();
-	}
-
-	private checkScreen(): void {
-		const MAX_WIDTH = 768;
-		this.screenWidth = window.innerWidth;
-		this.hasValidScreen = this.screenWidth < MAX_WIDTH;
+		this.smallScreen = window.innerWidth < 768;
 	}
 }
