5c547b26a57f82cb7cf2a8b9a0dfd028cbc2215f
Ife-PopOs <sunmolaife@gmail.com>
2023-03-19 20:18:13 -0600

    Changed to use typescript shorthand for classes

--- frontend/src/app/models/log-body.ts
+++ frontend/src/app/models/log-body.ts
@@ -1,16 +1,8 @@
 export class LogBody {
-	private timeStamp: number;
-	private logLevel: string = "";
-	private fileName: string;
-	private message: string;
-	private attachment: string
-
-	constructor(private _fileName: string, private _message: string, private _attachment = "") {
-		// loglevel will be set in the logger service
-		this.timeStamp = Date.now() / 1000;
-		this.fileName = _fileName;
-		this.message = _message;
-		this.attachment = _attachment;
+	// loglevel will be properly set in the logger service
+	constructor(private fileName: string, private message: string, private attachment = "",
+				private timeStamp = Date.now() / 1000, private logLevel = ""
+	) {
 	}
 
 	set _logLevel(logLevel: string) {
