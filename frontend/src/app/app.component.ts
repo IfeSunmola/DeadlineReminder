24e953499a5a0794e2cc0861cd96ff3040a00486
Ife-PopOs <sunmolaife@gmail.com>
2023-04-19 00:45:03 -0600

    Added pause development message

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,25 +17,24 @@ export class AppComponent implements OnInit {
 		this.smallScreen = window.innerWidth < 768;
 		const messages =
 			[
-				"1. This app is still under development.",
+				"1. Development of this site has been paused on April 19, 2023 because of my sudden interest in compiler theory and programming languages.",
 				"2. Only user authentication has been implemented.",
-				"3. This message will be shown regularly.",
+				"3. This message will be shown every 10 minutes.",
 				`4. Repo: 
 					<a target='_blank' href='https://github.com/IfeSunmola/DeadlineReminder'>
 						https://github.com/IfeSunmola/DeadlineReminder
 					</a>`
 			]
 		const snackbarData = {
-			title: "Note: March 19, 2023",
+			title: "Note: April 19th, 2023",
 			messages: messages,
 			action: "GOT IT"
 		}
-		timer(0, 600000).subscribe(
+		timer(0, 600000).subscribe( // show every 10 minutes
 			() => {
-				this.snackbarService.newFromComponent(snackbarData, 20000)
+				this.snackbarService.newFromComponent(snackbarData, 20000) // hide after 20 seconds
 			}
 		)
-
 	}
 
 	@HostListener('window:resize', ['$event'])
